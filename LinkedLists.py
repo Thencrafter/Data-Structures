d0460942672bffbf0b8a4ea0ab43ab91d1b9bfc7
Yerbol <yerbol_kosbayev@list.ru>
2025-05-13 02:32:45 +0500

    minor fixes

--- LinkedLists.py
+++ LinkedLists.py
@@ -34,29 +34,45 @@ class LinkedList:
         return counter
     
     def delete_node(self, index):
+        if self.head is None:   # Если список пуст
+            print("Список пуст.")
+            return
+
+        if index == 0:          # Удаление первого узла
+            self.head = self.head.next
+            return
+
         current = self.head
         counter = 0
-        if index == 0:
-            self.head = current.next
-        else:
-            while counter < index-1:
-                current = current.next
-                counter += 1
-            current.next = current.next.next
+        while current.next and counter < index - 1:
+            current = current.next
+            counter += 1
+
+        if current.next is None:  # Проверка выхода за границы
+            print("Индекс вне диапазона.")
+            return
+
+        current.next = current.next.next  # Удаляем узел
 
     def insert_node(self, index, data):
-        current = self.head
-        new_node = Node(data)
-        counter = 0
-        if index == 0:
+        new_node = Node(data)   # Создаем новый узел
+        if index == 0:          # Вставка в начало
+            new_node.next = self.head
             self.head = new_node
-            self.head.next = current
-        else:
-            while counter < index-1:
-                current = current.next
-                counter += 1
-            new_node.next = current.next
-            current = new_node
+            return
+
+        current = self.head
+        counter = 0
+        while current and counter < index - 1:
+            current = current.next
+            counter += 1
+
+        if current is None:     # Проверка выхода за границы
+            print("Индекс вне диапазона.")
+            return
+
+        new_node.next = current.next  # Связываем новый узел со следующим
+        current.next = new_node       # Вставляем новый узел
             
 
 newlinkedlist = LinkedList()
@@ -65,4 +81,4 @@ newlinkedlist.append(2)
 newlinkedlist.append(3)
 newlinkedlist.append(4)
 newlinkedlist.insert_node(0, 5)
-print(newlinkedlist.print_list())
+newlinkedlist.print_list() #тут ты итак принтишь
